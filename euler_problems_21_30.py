3736cff041b47599e35e4d3c025828e50fd0dc73
Monika Tercjak <monika.tercjak@gmail.com>
2020-03-20 14:06:23 +0100

    problem 22nd solved

--- euler_problems_21_30.py
+++ euler_problems_21_30.py
@@ -2,7 +2,7 @@ from pyfuncts import *
 
 
 # 21st problem ==============================================
-def problem21(max_number=100000):
+def problem21(max_number=10000):
     amicable_numbers = []
     for number in range(1, max_number + 1):
         number_fact = divisors(number)
@@ -12,8 +12,28 @@ def problem21(max_number=100000):
         if second_number == number and second_number != first_number and second_number not in amicable_numbers:
             amicable_numbers.append(second_number)
             amicable_numbers.append(first_number)
-            print(number, first_number, second_number)
     print(sum(amicable_numbers))
 
 
-problem21()
+# problem21()
+
+# 22nd problem ==============================================
+def problem22(filename='names.txt'):
+    file = open(filename, 'r')
+    names = file.read().replace('"', '')
+    names_list = sorted(names.split(','))
+    i = 0
+    result = 0
+    for name in names_list:
+        print(name)
+        i += 1
+        name_number = 0
+        for letter in name:
+            name_number += (ord(letter) - 64)
+            print(letter, ord(letter), name_number)
+        score = name_number * i
+        result += score
+    print(result)
+
+
+problem22()
