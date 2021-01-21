57f8148da41bc37d6ece1e1fc0f9cc4515900d76
sheenaze <tercjak@awst>
2021-01-21 21:03:24 +0100

    problem 31 efficient way added

--- euler_problems_31_40.py
+++ euler_problems_31_40.py
@@ -1,34 +1,35 @@
 from pyfuncts import *
 from itertools import permutations, combinations_with_replacement
-
+import numpy as np
 
 # 31st problem ==============================================
-def poly(a, b, c, d, e, f, g, h):
-    return a + 2 * b + 5 * c + 10 * d + 20 * e + 50 * f + 100 * g + 200 * h
+def problem31_inefficient():
+    coins = np.array([1, 2, 5, 10, 20, 50, 100, 200])
+    coin_to_find = coins[-1]
+    nums = 0
+    for a in range(0, coin_to_find // 1 + 1):
+        for b in range(0, coin_to_find // 2 + 1):
+            for c in range(0, coin_to_find // 5 + 1):
+                for d in range(0, coin_to_find // 10 + 1):
+                    for e in range(0, coin_to_find // 20 + 1):
+                        for f in range(0, coin_to_find // 50 + 1):
+                            for g in range(0, coin_to_find // 100 + 1):
+                                for h in range(0, coin_to_find // 200 + 1):
+                                    coeffs = np.array([a, b, c, d, e, f, g, h])
+                                    result = np.matmul(coeffs, coins)
+                                    if result == coin_to_find:
+                                        nums += 1
+                                        print(nums)
+    print(nums)
 
+def problem31_efficient(nums, max_num):
+    ways = [0] * (max_num + 1)
+    ways[0] = 1
+    for x in nums:
+        for i in range(x, max_num+1):
+            ways[i] += ways[i-x]
+    print(ways)
+    return ways[-1]
 
-def problem31():
-    coins = [1, 2, 5, 10, 20, 50, 100, 200]
-    coin_to_find = 200
-    ways_number = 0
-    file_name = 'numbers.txt'
-    file = open(file_name, 'w')
-    for h in range(0, 2):
-        for g in range(0, 2 - 2 * h + 1):
-            for f in range(0, 4 - 2 * g - 4 * h + 1):
-                for e in range(0, 10 - int(2.5 * f) - 5 * g - 10 * h + 1):
-                    for d in (0, 20 - 2 * e - 5 * f - 10 * g - 20 * h + 1):
-                        for c in range(0, 40 - 2 * d - 4 * e - 10 * f - 20 * g - 40 * h + 1):
-                            for b in range(0, 100 - int(2.5 * c) - 5 * d - 10 * e - 25 * f - 50 * g - 100 * h + 1):
-
-                                a_max = 200 - 2 * b - 5 * c - 10 * d - 20 * e - 50 * f - 100 * g - 200 * h + 1
-                            
-                                for a in range(0, a_max):
-                                    file.write(f'{[a, b, c, d, e, f, g, h]} \n')
-                                    print(a, b, c, d, e, f, g, h)
-                                    ways_number += 1 if poly(a, b, c, d, e, f, g, h) == coin_to_find else 0
-    file.close()
-    return ways_number
-
-
-print(problem31())
+nums = [1,2,5,10,20,50,100,200]
+# print(problem31_efficient(nums, max(nums)))
