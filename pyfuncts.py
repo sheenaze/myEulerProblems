6cfc5232d19c92fc6aa48ad0541e4f09bc2364c6
sheenaze <tercjak@awst>
2021-01-25 22:03:11 +0100

    exclude 1 as not a prime from isprime function

--- pyfuncts.py
+++ pyfuncts.py
@@ -47,6 +47,8 @@ def product(x):
 
 def isprime(num):
     boundary  = int(num ** (1/2)) + 1
+    if num == 1:
+        return False
     for i in range (2, boundary):
         if num % i == 0:
             return False
