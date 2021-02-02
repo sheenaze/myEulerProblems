0ba7b81b02f17e4754b403be4f344c61f171f96b
sheenaze <tercjak@awst>
2021-02-02 14:58:21 +0100

    problem 42 solved

--- euler_problems_41_50.py
+++ euler_problems_41_50.py
@@ -20,3 +20,22 @@ def problem41():
 
 # print(problem41())
 
+# 42nd problem ==============================================
+def problem42():
+    words = open("words.txt", "r")
+    words_list = words.read().split(',')
+    tnums = triangle_numbers(20)
+    is_triangle = 0
+    for word in words_list:
+        num_sum = 0
+        for letter in word:
+            if letter != '"':
+                num_sum += ord(letter) - 64
+        if num_sum in tnums:
+            is_triangle += 1
+
+    return is_triangle
+
+# print(problem42())
+
+
